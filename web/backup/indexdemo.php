3661cf423c7c9b390686bb23f0892f76bacdf90a
DKSchmidt <skekse29@hotmail.com>
2016-04-27 12:31:55 -0700

    Added web stuff

---
+++ web/backup/indexdemo.php
@@ -0,0 +1,17 @@
+<?php
+  print <<< EOT
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>EEE 174 Lab 6</title>
+</head>
+<body>
+<h1>Daniel</h1>
+<h1>EEE 174 Lab 6: Raspberry Pi</h1>
+<h1>This webpage is run from a Raspberry Pi computer.</h1>
+</body>
+</html>
+EOT;
+
+?>
---
+++ web/scripts/script1.js
@@ -0,0 +1,28 @@
+var hexDigits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+function convert() {
+    var num = Number(document.dec.decNum.value);
+	var select = document.dec.select.value;
+	document.getElementById("binNum").innerHTML = "";
+
+	var result = "";
+	if (select === 'binary') {
+	    for (var i = 1; i < 17; i++) {
+    	    if (num / Math.pow(2, (16 - i)) >= 1) {
+		        result = result + "1";
+                num = num - Math.pow(2, (16 - i));
+		    } else { result = result + "0";	}
+	    }
+	} else if (select === 'hexadecimal') {
+	    for (var j = 1; j < 5; j++) {
+		    var powSixt = Math.pow(16, (4 - j));
+			var hDg = Math.floor(num / powSixt);
+    	    if (num / powSixt >= 1) {
+		        result = result + hexDigits[hDg - 1];
+                num = num - powSixt * hDg;
+		    } else { result = result + "0";	}
+	    }
+	}
+	
+	document.getElementById("binNum").innerHTML += result;
+}
