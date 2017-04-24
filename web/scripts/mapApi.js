73dcdfeb27c29ea84157e375a225a08cdb8a08fc
Daniel Schmidt <skekse29@hotmail.com>
2017-04-23 20:58:40 -0700

    Map page can take lat and lng as input.

---
+++ web/scripts/mapApi.js
@@ -0,0 +1,14 @@
+//var coords = {lat: null, lng: null};
+var coords = {lat: -25.363, lng: 131.044};
+
+function initMap() {
+    var coords = {lat: Number(document.getElementById('latIn').value),
+                  lng: Number(document.getElementById('lngIn').value)}
+    var map = new google.maps.Map(document.getElementById('map'), {
+                                  zoom: 4,
+                                  center: coords});
+    var marker = new google.maps.Marker({position: coords,
+                                         map: map });
+}
+
+
