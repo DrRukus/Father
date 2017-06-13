a49f17473a538c39cea80e24c900b5983291117d
dschmidt <skekse29@hotmail.com>
2017-06-12 22:17:55 -0700

    Fixed some bugs in merge function.

--- python/algos/mergeSort.py
+++ python/algos/mergeSort.py
@@ -11,22 +11,38 @@ def merge(leftList, rightList):
     leftPtr = 0
     rightPtr = 0
     loopCount = len(leftList) + len(rightList)
-    while loopCount > 0:
-        if leftList[leftPtr] > rightList[rightPtr]:
-            merger.append(leftList[leftPtr])
-            if leftPtr >= len(leftList) - 1:
-                break
-            leftPtr += 1
-        else:
-            merger.append(rightList[rightPtr])
-            if rightPtr >= len(rightList) - 1:
-                break
-            rightPtr += 1
-        loopCount -= 1
-    if leftPtr < len(leftList) - 1:
-        merger.extend(leftList[leftPtr:len(leftList)])
-    elif rightPtr < len(rightList) - 1:
-        merger.extend(rightList[rightPtr:len(rightList)])
-    return merger
+    if loopCount > 2:
+        while loopCount > 0:
+            #print "Left Ptr: {}; Right Ptr: {}".format(leftPtr, rightPtr)
+            if leftList[leftPtr] > rightList[rightPtr]:
+                #print "\t{} > {}".format(leftList[leftPtr], rightList[rightPtr])
+                merger.append(leftList[leftPtr])
+                if leftPtr >= len(leftList) - 1:
+                    if rightPtr == len(rightList) - 1:
+                        merger.append(rightList[-1])
+                    break
+                leftPtr += 1
+            else:
+                #print "\t{} <= {}".format(leftList[leftPtr], rightList[rightPtr])
+                merger.append(rightList[rightPtr])
+                if rightPtr >= len(rightList) - 1:
+                    if leftPtr == len(leftList) - 1:
+                        merger.append(leftList[-1])
+                    break
+                rightPtr += 1
+            loopCount -= 1
+        if leftPtr < len(leftList) - 1:
+            merger.extend(leftList[leftPtr:len(leftList)])
+        elif rightPtr < len(rightList) - 1:
+            merger.extend(rightList[rightPtr:len(rightList)])
+        return merger
+    else:
+        return sorted([leftList[0], rightList[0]], reverse=True)
+
+print merge([6, 3, 2], [5, 4, 1])
 
 print merge([6, 4, 3], [5, 2, 1])
+
+print merge([6], [4])
+
+print merge([6, 4], [4, 3])
