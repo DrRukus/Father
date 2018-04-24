c74ea952f606fe8f585e427951dcb42c14896c2a
Daniel Schmidt <dkschmidt29@gmail.com>
2018-04-23 21:27:44 -0700

    Completed basic REST API with item delete and RequestParser.

--- python/flask/code/app.py
+++ python/flask/code/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -16,10 +16,15 @@ items = []
 
 
 class Item(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument('price', 
+		type=float,
+		required=True,
+		help='This field cannot be left blank!'
+	)
+
 	@jwt_required()
 	def get(self, name):
-		#item = next(filter(lambda i: i['name'] == name, items), None)
-		#return item, 200 if item else 404 
 		return {'item': next(filter(lambda i: i['name'] == name, items), None)}
 
 	def post(self, name):
@@ -27,11 +32,28 @@ class Item(Resource):
 			item = {'message': 'An item with name "{}" already exists.'.format(name)}
 			status = 400
 		else:
-			data = request.get_json()
+			data = Item.parser.parse_args()
+
 			item = {'name': name, 'price': data['price']}
 			items.append(item)
 			status = 201
 		return item, status
+
+	def delete(self, name):
+		global items
+		items = list(filter(lambda x: x['name'] != name, items))
+		return {'message': 'Item deleted'}
+
+	def put(self, name):
+		data = Item.parser.parse_args()
+
+		item = next(filter(lambda x: x['name'] == name, items), None)
+		if item is None:
+			item = {'name': name, 'price': data['price']}
+			items.append(item)
+		else:
+			item.update(data)
+		return item
 
 
 class ItemList(Resource):
@@ -42,3 +64,4 @@ api.add_resource(Item, '/item/<string:name>') # http://127.0.0.1:5000/item/
 api.add_resource(ItemList, '/items')
 
 app.run(port=5000, debug=True)
+
